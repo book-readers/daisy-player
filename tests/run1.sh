b44a25783e640d4aebede132d7dc4b7acf3ea2a2
Colomban Wendling <cwendling@hypra.fr>
2023-09-20 22:39:39 +0200

    Add basic format reading tests
    Verify that the "spine" (play order) is stable, by dumping it from
    test books.
    
    Books are from various locations, removing any actual recording not
    necessary for the tests, and possibly copyrighted.  All that is left
    should be assumed meta-data, and be fair-use.

---
+++ tests/run1.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -xue
+
+export LC_ALL=C
+
+test -d "$abs_srcdir"
+
+if test "$1" = '--generate'; then
+  generate=true
+  shift
+  actual="$2"
+else
+  generate=false
+  trap 'rm -f "$actual"' EXIT INT QUIT TERM
+  actual=$(mktemp)
+fi
+
+expected="$2"
+input="${expected%.*}"
+
+# try and make abs_srcdir safe to use as a sed expression
+sed_safe_abs_srcdir=$(printf %s "${abs_srcdir}" | sed 's%[].\[*]%\\\0%g')
+
+# run daisy-player -D
+"$1" -D "$input" | sed "s%${sed_safe_abs_srcdir}%%g;s%/[.]/%%" > "$actual"
+
+# compare the result with the expectations
+$generate || diff -u "$expected" "$actual"
